f332b88cd296fcd51f35890d0d8c31d8f25eb0d7
TheAlanGan <alan.gan@outlook.com>
2022-05-22 20:14:46 -0400

    Fixed bugs in MCMC code. Got it working.
    Although the markov chain does not converge well due to small step
    sizes. And if we do larger step sizes, then the acceptance rate is too
    small... The high dimensionality of the parameters seems to be a
    problem.

--- Kalman filter (MCMC).R
+++ Kalman filter (MCMC).R
@@ -7,7 +7,7 @@ library(truncnorm)
 
 ### Parameters
 
-n <- 100 # sample size
+n <- 20 # sample size
 H <- 1   # Data covariate
 
 true.R <- 0.1 # Data Variance / Measurement Error
@@ -23,12 +23,16 @@ M <- 0.7
 
 set.seed(123)
 nu <- rnorm(n, 0, true.Q)
-x <- rep(NA, n)
+x <- rep(NA, n+1)
 x[1] <- rnorm(1)
+x0.true <- x[1]
 
-for (t in 2:n) {
-  x[t] <- (true.M)*x[t-1] + nu[t]
+for (t in 2:(n+1)) {
+  x[t] <- (true.M)*x[t-1] + nu[t-1]
 }
+
+x <- x[-1]
+x.true <- x
 
 epsilon <- rnorm(n, 0, true.R)
 y <- H * x + epsilon
@@ -37,7 +41,6 @@ y <- H * x + epsilon
 # remove <- 30:43
 
 #y[remove] <- NA
-
 
 
 
@@ -56,10 +59,11 @@ MH <- function(N.mc) {
   M <- rep(NA, N.mc)
   sigma2eta <- rep(NA, N.mc)
   
-  set.seed(444)
-  x[,1] <- rnorm(n+1) # Initialization
-  M[1] <- 1
-  sigma2eta[1] <- 1
+  #set.seed(444)
+  x[1,1] <- x0.true
+  x[2:(n+1),1] <- x.true #rnorm(n+1, 0, 0.1) # Initialization
+  M[1] <- 0.7
+  sigma2eta[1] <- 0.5
   
   for (i in 2:N.mc) {
     #y <- sample.prop(x[i-1], ...)
@@ -75,10 +79,10 @@ MH <- function(N.mc) {
     numerator <- eval.density(y, prop$x, prop$M, prop$sigma2eta)
     denominator <- eval.density(y, x[,i-1], M[i-1], sigma2eta[i-1])
     
-    if (numerator < 1e-10) {
+    if (numerator < 1e-15) {
       rho <- 0
     } else {
-      rho <- numertor / denominator
+      rho <- numerator / denominator
     }
     
     #AR <- min(rho,1)
@@ -109,10 +113,10 @@ eval.density <- function(y, x, M, sigma2eta) { # Target density
 }
 
 
-v <- c(0.5, 0.1, 0.1) # step sizes for random walk
+v <- c(0.05, 0.01, 0.01) # step sizes for random walk
 
 rand.walk.prop <- function(x, M, sigma2eta) { # Proposal
-  n <- length(x)
+  n <- length(x)-1
   x.new <- rnorm(n+1, mean = x, sd = v[1])
   M.new <- rnorm(1, mean = M, sd = v[2])
   sigma2eta.new <- rtruncnorm(1, a = 0, mean = sigma2eta, sd = v[3])
@@ -126,6 +130,30 @@ rand.walk.dens <- function(x.new, M.new, sigma2eta.new, x, M, sigma2eta) { # Pro
 }
 
 
-mc <- MH(1000)
+mc <- MH(100000)
+mc$accept.rate
+
+
+# plot(mc$M.path)
+# hist(mc$M.path, breaks = 50)
+# acf(mc$M.path)
+
+# plot(mc$sigma2eta.path)
+# hist(mc$sigma2eta.path, breaks = 50)
+# acf(mc$sigma2eta.path)
+
+filter.x.mcmc <- apply(mc$x.path, 1, mean)
+filter.x.2p5 <- apply(mc$x.path, 1, quantile, probs = c(0.025))
+filter.x.97p5 <- apply(mc$x.path, 1, quantile, probs = c(0.975))
+
+plot(x.true, col = 'orange', type = 'l', ylim = c(min(x)-0.1, max(x)+0.1), xlab = 't')
+points(y, col = 'red', cex = 0.8, pch = 16)
+lines(filter.x.mcmc[-1], col = "blue2", lty = 2)
+lines(filter.x.2p5[-1], col = "lightblue", lty = 2)
+lines(filter.x.97p5[-1], col = "lightblue", lty = 2)
+legend("topright", legend = c("Truth", "Data", "Filter"),
+       lty = c(1, NA, 2),
+       pch = c(NA, 16, NA),
+       col = c("orange", "red", "blue2"))
 
 
